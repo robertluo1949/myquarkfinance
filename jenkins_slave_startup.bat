6f977d3e085a87faf8e269c32a6ab255430c2010
Robert <robert_luo1949@163.com>
2016-08-30 16:01:25 +0800

    add @echo off ，

--- jenkins_slave_startup.bat
+++ jenkins_slave_startup.bat
@@ -1,8 +1,10 @@
+@echo off  
 REM -Dfile.encoding="UTF-8"
 REM SET JAVA_OPTS="-server -Xms800m -Xmx800m -XX:PermSize=64M -XX:MaxNewSize=256M -XX:MaxPermSize=128M -Djava.awt.headless=true"
 TITLE=jenkins-slave:slave_win_97”
 color 47
-javaw -jar slave.jar -jnlpUrl http://172.26.186.83:9999/computer/slave_win_97/slave-agent.jnlp -secret 4920340777f46faad15e87fe6671696bc88de7bbdae26f3862c2bb47ff908ec4  &
-:echo OFF "startup javaw successful"
+@echo on
+javaw -jar slave.jar -jnlpUrl http://172.26.186.83:9999/computer/slave_win_97/slave-agent.jnlp -secret 4920340777f46faad15e87fe6671696bc88de7bbdae26f3862c2bb47ff908ec4  
+@echo  "startup javaw successful"
 TASKLIST |FIND "javaw.exe"
 PAUSE
